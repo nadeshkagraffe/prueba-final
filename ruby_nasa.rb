75578ea7c1bab20cd42d0aae991bb4512b9fff08
nadeshkagraffe <nadeshkagraffe@gmail.com>
2021-03-25 23:54:34 -0300

    metodo biud_web_page, creando la pagina index html

--- ruby_nasa.rb
+++ ruby_nasa.rb
@@ -13,3 +13,28 @@ end
 
 
 hash = request("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=","vHz2dBH3vhuJg4dherN6dVSogYJOV0uGJPugQ8Xe")
+def buid_web_page (hash)
+    
+    img =hash["photos"].collect do |i|
+         i["img_src"]
+            end
+         html1 = 
+        '<html>
+        <head>
+        </head>
+        <body>
+        <ul>'
+    
+    img.each do |img|
+        html1 += "<li><img src=\"#{img}\"></li>\n"
+    end
+
+    html1 += '</ul>
+             </body>
+            </html>'
+
+    File.write("index.html", html1)
+end
+
+
+buid_web_page (hash)
